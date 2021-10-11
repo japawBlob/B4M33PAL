120e95a71f9b89cb9bba030a4817b8bbd300c142
Jakub Jira <jakub.jira@protonmail.com>
2021-10-12 01:08:06 +0200

    1UtilPipes - assigning to hubs not working with pub06+

--- 1UtilPipes/cmake-build-debug/CMakeFiles/1UtilPipes.dir/DependInfo.cmake
+++ 1UtilPipes/cmake-build-debug/CMakeFiles/1UtilPipes.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/japaw/Codes/PAL/1UtilPipes/main.cpp" "/home/japaw/Codes/PAL/1UtilPipes/cmake-build-debug/CMakeFiles/1UtilPipes.dir/main.cpp.o"
+  "/home/japaw/Codes/B4M33PAL/1UtilPipes/main.cpp" "/home/japaw/Codes/B4M33PAL/1UtilPipes/cmake-build-debug/CMakeFiles/1UtilPipes.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- 1UtilPipes/main.cpp
+++ 1UtilPipes/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
-
+#include <queue>
+#include <functional>
+#include <utility>
 //int map [numberOfFarms][numberOfFarms];
 
 std::vector<std::vector<int>> map;
@@ -16,75 +19,147 @@ struct farm {
 
 std::vector<struct farm> farms;
 
-int main(int argc, char const *argv[])
-{
-	int numberOfFarms, numberOfPipes;
-	std::cout << "Hello this is my first PAL Program\n";
-	scanf("%i %i", &numberOfFarms, &numberOfPipes);
+int disconnectedFarms = 0;
 
-	//struct farm farms [numberOfFarms];
+void printMap();
+void printNeighbours(int name);
 
-	/*map.reserve(numberOfFarms);
-	for (std::vector<int> line : map){
-		line.reserve(numberOfFarms);
-	}*/
+int main(int argc, char const *argv[]) {
+    int numberOfFarms, numberOfPipes;
+    std::cout << "Hello this is my first PAL Program\n";
+    scanf("%i %i", &numberOfFarms, &numberOfPipes);
 
-	//int map [numberOfFarms][numberOfFarms];
+    //struct farm farms [numberOfFarms];
 
-	// !!!!!!! farms.assign(numberOfFarms, static_cast<struct farm>(0));
-	map.assign(numberOfFarms, std::vector<int> (numberOfFarms, 0));
+    /*map.reserve(numberOfFarms);
+    for (std::vector<int> line : map){
+        line.reserve(numberOfFarms);
+    }*/
 
-	for (int i = 0; i < numberOfPipes; i++){
-		int from, to, cost;
-		scanf("%i %i %i", &from, &to, &cost);
-		//std::cin << temp1 << temp2 << temp3;
-		map[from][to] = cost;
-		map[to][from] = cost;
-	}
-	int numberOfHubs;
-	scanf("%i", &numberOfHubs);
+    //int map [numberOfFarms][numberOfFarms];
+    farms.reserve(numberOfFarms);
+    struct farm temp_farm = {99999, -1, -1, false};
+    for (int i = 0; i < numberOfFarms; ++i) {
+        temp_farm.name = i;
+        farms.push_back(temp_farm);
+    }
 
+    map.assign(numberOfFarms, std::vector<int>(numberOfFarms, 0));
+
+    for (int i = 0; i < numberOfPipes; i++) {
+        int from, to, cost;
+        scanf("%i %i %i", &from, &to, &cost);
+        //std::cin << temp1 << temp2 << temp3;
+        map[from][to] = cost;
+        map[to][from] = cost;
+        farms[from].neighbours.push_back(to);
+        farms[to].neighbours.push_back(from);
+    }
+    int numberOfHubs;
+    scanf("%i", &numberOfHubs);
+    std::vector<int> hubs;
+    for (int i = 0; i < numberOfHubs; i++) {
+        int newHub;
+        scanf("%i", &newHub);
+        farms[newHub].distToHub = 0;
+        farms[newHub].homeHub = newHub;
+        farms[newHub].isConnected = true;
+        hubs.push_back(newHub);
+    }
+    /*for (int i = 0; i < numberOfHubs; ++i) {
+        std::cout << hubs[i] << " " ;
+    }
+    std::cout << "\n";
+
+    for (int i = 0; i < numberOfFarms; ++i) {
+        std::cout << farms[i].distToHub << " ";
+    }
+    std::cout << "\n";*/
+
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> prioQueue;
+    for(int currentHub : hubs){
+        prioQueue.push(std::make_pair(farms[currentHub].distToHub, currentHub));
+        //printNeighbours(hubs[currentHub]);
+        while (!prioQueue.empty()) {
+            auto currentFarm = prioQueue.top();
+            prioQueue.pop();
+            //std::cout << "pop " << currentNode.first << " " << currentNode.second << "\n";
+            for (int neighbour: farms[currentFarm.second].neighbours) {
+                if (farms[neighbour].distToHub > currentFarm.first + map[neighbour][currentFarm.second]) {
+                    /*if (farms[neighbour].homeHub == -2) {
+                        disconnectedFarms--;
+                    }*/
+                    farms[neighbour].homeHub = currentHub;
+                    farms[neighbour].isConnected = true;
+                    farms[neighbour].distToHub = currentFarm.first + map[neighbour][currentFarm.second];
+                    prioQueue.push(std::make_pair(farms[neighbour].distToHub, neighbour));
+                    continue;
+                }
+                if (farms[neighbour].distToHub == currentFarm.first + map[neighbour][currentFarm.second]) {
+                    /*if (farms[neighbour].homeHub != -2) {
+                        disconnectedFarms++;
+                    }*/
+                    farms[neighbour].homeHub = -2;
+                    farms[neighbour].isConnected = false;
+                    prioQueue.push(std::make_pair(farms[neighbour].distToHub, neighbour));
+                }
+            }
+        }
+    }
 	for (int i = 0; i < numberOfFarms; ++i){
-		farms[i].distToHub = 99999999;
-		farms[i].isConnected = false;
-
-	}
-
-	for(int i = 0; i < numberOfHubs; i++){
-		int temp1;
-		scanf("%i", &temp1);
-		farms[temp1].distToHub = 0;
-	}
-
-	//"DRONI KOKOTE"
-
-
-
-
-
-
-	for (int i = 0; i < numberOfFarms; ++i){
-		std::cout << farms[i].distToHub << " ";
+		std::cout << std::setw(2) << i << " ";
 	}
 	std::cout << "\n";
-
-
-
-
-	std::cout << "  ";
 	for (int i = 0; i < numberOfFarms; ++i){
-		std::cout << i << " ";
+		std::cout << std::setw(2) << farms[i].distToHub << " ";
+	}
+    std::cout << "\n";
+    for (int i = 0; i < numberOfFarms; ++i){
+        std::cout << std::setw(2) << farms[i].homeHub << " ";
+    }
+	std::cout << "\n";
+    std::cout << disconnectedFarms << "\n";
+    int numberOfDisconections = 0;
+    for(struct farm farmm : farms){
+        if(farmm.homeHub == -2){
+            numberOfDisconections++;
+        }
+    }
+    std::cout << numberOfDisconections << "\n";
+
+
+/*
+	printMap();
+	
+	printNeighbours(5);
+	printNeighbours(6);
+	printNeighbours(19);
+	printNeighbours(14);
+*/
+
+	return 0;
+}
+
+void printNeighbours(int name){
+	std::cout << "Hello my name is " << name << " and my neighbours are:\n";
+	for (int neighbour : farms[name].neighbours){
+		std::cout << "neighbour\'s name: " << neighbour << " | distance: " << map[name][neighbour] << "\n";
+	}
+}
+
+void printMap(){
+	std::cout << "   ";
+	int numberOfFarms = map.size();
+	for (int i = 0; i < numberOfFarms; ++i){
+		std::cout << std::setw(2) << i << " ";
 	}
 	std::cout << "\n";
 	for (int i = 0; i < numberOfFarms; ++i){
-		std::cout << i << " ";
+		std::cout << std::setw(2) << i << " ";
 		for (int j = 0; j < numberOfFarms; ++j){
-			std::cout << map[i][j] << " ";
+			std::cout << std::setw(2) << map[i][j] << " ";
 		}
 		std::cout << "\n";
 
 	}
-	
-
-	return 0;
 }
