94e3ea9c2c45bd6138d642a3f84448cbf3af2415
Jakub Jira <jakub.jira@protonmail.com>
2021-10-26 01:04:05 +0200

    Tarjan fixed, condensated graph avaits implementation

--- 2Conveyor/main.cpp
+++ 2Conveyor/main.cpp
@@ -5,11 +5,21 @@
 struct point {
     int sscMember;
     int lowLink;
+    int index;
     bool sscResolved;
+    bool inStack;
     std::vector<int> neighbourPoints;
+    std::vector<int> childOf;
 };
+std::vector<std::vector<int>> SSCs;
 
 std::vector<struct point> points;
+
+int numberOfSSCs = 0;
+
+std::stack<int> tarjanStack;
+
+int recuTarjan(int currentPoint/*, std::stack<int>& tarjanStack*/);
 
 int main (){
     std::cout << "hello this is my second pal program\n";
@@ -25,15 +35,18 @@ int main (){
         int from, to;
         scanf("%i %i", &from, &to);
         points[from].sscMember = -1;
+        points[from].index = 0;
         points[from].sscResolved = false;
+        points[from].inStack = false;
         points[from].lowLink = 99999999;
         points[from].neighbourPoints.push_back(to);
+        points[to].childOf.push_back(from);
     }
 
-    std::stack<int> tarjanStack;
+
     int lowLink = 1;
     for (int i = 0; i < numberOfPoints; ++i) {
-        if(!points[i].sscResolved){
+        /*if(!points[i].sscResolved){
             points[i].lowLink = lowLink++;
             tarjanStack.push(i);
             while (!tarjanStack.empty()){
@@ -50,13 +63,23 @@ int main (){
                     }
                 }
             }
-        }
+        }*/
+        recuTarjan(i/*, tarjanStack*/);
+
     }
 
 
 
 
-
+/*
+    for (int i = 0; i < numberOfPoints; ++i) {
+        std::cout << i << "   ";
+        for (int j = 0; j < points[i].neighbourPoints.size(); ++j) {
+            std::cout << points[i].neighbourPoints[j] << " ";
+        }
+        std::cout << std::endl;
+    }
+*/
     for (int i = 0; i < numberOfPoints; ++i) {
         std::cout << i << "   ";
         for (int j = 0; j < points[i].neighbourPoints.size(); ++j) {
@@ -65,14 +88,62 @@ int main (){
         std::cout << std::endl;
     }
 
+    for (int i = 0; i < numberOfPoints; ++i) {
+        std::cout << i << " " << points[i].sscMember << std::endl;
+    }
+    /*int i = 0;
+    for (auto vec : SSCs) {
+        //std::cout << i++ << "  \n";
+        for (auto blob : vec) {
+            std::cout << blob << " ";
+        }
+        std::cout << "\n";
+    }*/
+    for (int i = 0; i < points.size(); ++i) {
+        std::cout << points[i].sscMember << " ";
+    } std::cout << std::endl;
+
     return 0;
 }
 
-int recuTarjan(int currentPoint){
+int recuTarjan(int currentPoint/*, std::stack<int>& tarjanStack*/){
+    static int index = 0;
+    points[currentPoint].lowLink = index;
+    points[currentPoint].index = index;
+    tarjanStack.push(currentPoint);
+    points[currentPoint].inStack = true;
+    index++;
+
     for (int neighbour : points[currentPoint].neighbourPoints){
-        if(!points[neighbour].sscResolved && points[neighbour].sscMember != -1){
-            currentPoint
+        if (points[neighbour].index == 0){
+            recuTarjan(neighbour/*, tarjanStack*/);
+            points[currentPoint].lowLink = std::min(points[currentPoint].lowLink, points[neighbour].lowLink);
+        } else if (points[neighbour].inStack) {
+            points[currentPoint].lowLink = std::min(points[currentPoint].lowLink, points[neighbour].index);
         }
+    }
+    if(points[currentPoint].lowLink == points[currentPoint].index){
+        std::vector<int> blob;
+        SSCs.push_back(blob);
+        int riley;
+        do{
+            riley = tarjanStack.top();
+            tarjanStack.pop();
+            if (!points[riley].sscResolved){
+                points[riley].sscMember = numberOfSSCs;
+                points[riley].sscResolved = true;
+            }
+            points[riley].inStack = false;
+            SSCs[numberOfSSCs].push_back(riley);
+        } while (currentPoint != riley);
+        if (SSCs[numberOfSSCs].size() > 1){
+            for (auto blob : SSCs[numberOfSSCs]) {
+                //find(vec.begin(), vec.end(), elem) != vec.end()
+                //points[currentPoint]
+            }
+
+        }
+        numberOfSSCs++;
     }
     return 0;
 }
